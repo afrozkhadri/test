a5df4c6988762bcef6dd032f3c08179aedfa52cb
afrozkhadri <32006763+afrozkhadri@users.noreply.github.com>
2018-12-12 12:51:06 +0530

    Add files via upload

---
+++ BooleanCount.py
@@ -0,0 +1,12 @@
+def count_boolean(b1, b2, b3):
+    myFlag=(b1||b2)&&(b3||b1)&&(b2||b3)
+   if(b4=False) :
+        return False
+   }
+    else :
+	return True
+	   
+	   
+   }
+
+print(count_boolean(False,False,True))
